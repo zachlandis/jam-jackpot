5280400878c3ba4c024f0a6ca6d8ef576cb5d8f2
Zach <zachlandis91@gmail.com>
2023-10-31 11:06:46 -0600

    Fix 'Stack Level Too Deep' issue (in serializers)

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,5 +1,5 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :user, :entry_date, :winner, :user_id, :giveaway_id, :giveaway
+  attributes :id, :entry_date, :winner, :user_id
 
   belongs_to :user
   belongs_to :giveaway
