a30e23813f2e4d69da87fdb8c91448002a65c751
Zach <zachlandis91@gmail.com>
2024-01-15 13:37:28 -0700

    Clean up Signup component

--- client/src/features/Auth/SignUp.js
+++ client/src/features/Auth/SignUp.js
@@ -1,64 +1,62 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { signUpUser } from '../Reducers/UsersSlice'
-import { Navigate } from "react-router-dom";
-
+import { useDispatch } from 'react-redux';
+import { signUpUser } from '../Reducers/UsersSlice';
+import { useNavigate } from 'react-router-dom';
 
 function SignUpForm() {
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    city: '',
+    state: '',
+    email: '',
+    password: '',
+  });
 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const isLoggedIn = useSelector((state) => state.users.isLoggedIn);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    const userData = {
-      email,
-      phone,
-      password,
-      first_name: firstName,
-      last_name: lastName,
-      user_city: city,
-      user_state: state,
-      admin: false,
-    };
-
-    dispatch(signUpUser(userData));
-    
-    setEmail('');
-    setPhone('');
-    setPassword('');
-    setFirstName('');
-    setLastName('');
-    setCity('');
-    setState('');
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  if (isLoggedIn) {
-    return <Navigate to="/login" />;
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    dispatch(signUpUser(formData));
+
+    setFormData({
+      email: '',
+      phone: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      city: '',
+      state: '',
+    });
+    navigate('/login');
+  };
 
   return (
     <div className="signup-container">
       <div className="card">
         <div className="card-body">
           <h2 className="card-title text-center">Sign Up</h2>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <input
                 type="text"
                 className="form-control"
                 placeholder="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
               />
             </div>
             <div className="form-group">
@@ -66,8 +64,10 @@ function SignUpForm() {
                 type="text"
                 className="form-control"
                 placeholder="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
               />
             </div>
             <div className="form-group">
@@ -75,8 +75,10 @@ function SignUpForm() {
                 type="text"
                 className="form-control"
                 placeholder="City"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
+                name="city"
+                value={formData.city}
+                onChange={handleChange}
+                required
               />
             </div>
             <div className="form-group">
@@ -85,98 +87,105 @@ function SignUpForm() {
                 name="state"
                 type="dropdown"
                 placeholder="State"
-                value={state}
-                onChange={(e) => setState(e.target.value)}
+                value={formData.state}
+                onChange={handleChange}
                 required
-                  >
-                    <option value="">Select State</option> 
-                    <option value="Alabama">Alabama</option>
-                    <option value="Alaska">Alaska</option>
-                    <option value="Arizona">Arizona</option>
-                    <option value="Arkansas">Arkansas</option>
-                    <option value="California">California</option>
-                    <option value="Colorado">Colorado</option>
-                    <option value="Connecticut">Connecticut</option>
-                    <option value="Deleware">Deleware</option>
-                    <option value="Florida">Florida</option>
-                    <option value="Georgia">Georgia</option>
-                    <option value="Hawaii">Hawaii</option>
-                    <option value="Idaho">Idaho</option>
-                    <option value="Illinois">Illinois</option>
-                    <option value="Indiana">Indiana</option>
-                    <option value="Iowa">Iowa</option>
-                    <option value="Kentucky">Kentucky</option>
-                    <option value="Louisiana">Louisiana</option>
-                    <option value="Maine">Maine</option>
-                    <option value="Maryland">Maryland</option>
-                    <option value="Massachusetts">Massachusetts</option>
-                    <option value="Michigan">Michigan</option>
-                    <option value="Minnesota">Minnesota</option>
-                    <option value="Mississippi">Mississippi</option>
-                    <option value="Missouri">Missouri</option>
-                    <option value="Montana">Montana</option>
-                    <option value="Nebraska">Nebraska</option>
-                    <option value="Nevada">Nevada</option>
-                    <option value="New Hampshire">New Hampshire</option>
-                    <option value="New Jersey">New Jersey</option>
-                    <option value="New Mexico">New Mexico</option>
-                    <option value="New York">New York</option>
-                    <option value="North Carolina">North Carolina</option>
-                    <option value="North Dakota">North Dakota</option>
-                    <option value="Ohio">Ohio</option>
-                    <option value="Okalhoma">Okalhoma</option>
-                    <option value="Oregon">Oregon</option>
-                    <option value="Pennyslvania">Pennyslvania</option>
-                    <option value="Rhode Island">Rhode Island</option>
-                    <option value="South Carolina">South Carolina</option>
-                    <option value="South Dakota">South Dakota</option>
-                    <option value="Tennessee">Tennessee</option>
-                    <option value="Texas">Texas</option>
-                    <option value="Utah">Utah</option>
-                    <option value="Vermont">Vermont</option>
-                    <option value="Virginia">Virginia</option>
-                    <option value="Washington">Washington</option>
-                    <option value="Washington D.C.">Washington D.C.</option>
-                    <option value="West Virginia">West Virginia</option>
-                    <option value="Wisconsin">Wisconsin</option>
-                    <option value="Wyoming">West Virginia</option>
-                    </select>
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="phone"
-                      className="form-control"
-                      placeholder="Phone"
-                      value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                  <div className="text-center">
-                    <button type="submit" className="btn btn-primary">
-                      Sign Up
-                    </button>
-                  </div>
-                </form>
+              >
+                <option value="">Select State</option>
+                <option value="">Select State</option> 
+                <option value="Alabama">Alabama</option>
+                <option value="Alaska">Alaska</option>
+                <option value="Arizona">Arizona</option>
+                <option value="Arkansas">Arkansas</option>
+                <option value="California">California</option>
+                <option value="Colorado">Colorado</option>
+                <option value="Connecticut">Connecticut</option>
+                <option value="Deleware">Deleware</option>
+                <option value="Florida">Florida</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Hawaii">Hawaii</option>
+                <option value="Idaho">Idaho</option>
+                <option value="Illinois">Illinois</option>
+                <option value="Indiana">Indiana</option>
+                <option value="Iowa">Iowa</option>
+                <option value="Kentucky">Kentucky</option>
+                <option value="Louisiana">Louisiana</option>
+                <option value="Maine">Maine</option>
+                <option value="Maryland">Maryland</option>
+                <option value="Massachusetts">Massachusetts</option>
+                <option value="Michigan">Michigan</option>
+                <option value="Minnesota">Minnesota</option>
+                <option value="Mississippi">Mississippi</option>
+                <option value="Missouri">Missouri</option>
+                <option value="Montana">Montana</option>
+                <option value="Nebraska">Nebraska</option>
+                <option value="Nevada">Nevada</option>
+                <option value="New Hampshire">New Hampshire</option>
+                <option value="New Jersey">New Jersey</option>
+                <option value="New Mexico">New Mexico</option>
+                <option value="New York">New York</option>
+                <option value="North Carolina">North Carolina</option>
+                <option value="North Dakota">North Dakota</option>
+                <option value="Ohio">Ohio</option>
+                <option value="Okalhoma">Okalhoma</option>
+                <option value="Oregon">Oregon</option>
+                <option value="Pennyslvania">Pennyslvania</option>
+                <option value="Rhode Island">Rhode Island</option>
+                <option value="South Carolina">South Carolina</option>
+                <option value="South Dakota">South Dakota</option>
+                <option value="Tennessee">Tennessee</option>
+                <option value="Texas">Texas</option>
+                <option value="Utah">Utah</option>
+                <option value="Vermont">Vermont</option>
+                <option value="Virginia">Virginia</option>
+                <option value="Washington">Washington</option>
+                <option value="Washington D.C.">Washington D.C.</option>
+                <option value="West Virginia">West Virginia</option>
+                <option value="Wisconsin">Wisconsin</option>
+                <option value="Wyoming">West Virginia</option>
+              </select>
             </div>
+            <div className="form-group">
+              <input
+                type="tel"
+                className="form-control"
+                placeholder="Phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="text-center">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
   );
 }
