8bd87e36dc7190f48b440c7f0c8acaf3adb6c3ea
Zach <zachlandis91@gmail.com>
2024-01-18 10:20:56 -0700

    Clean up UserPrizes component

--- client/src/features/Users/UserPrizes.js
+++ client/src/features/Users/UserPrizes.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function UserPrizes({ currentUser }) {
   return (
     <div className="prizes-section">
-        <h2>Your Prizes</h2>
-        {currentUser.prev_wins && currentUser.prev_wins.length > 0 ? (
-            <ul>
-            {currentUser.prev_wins.map((win) => (
-                <li key={win.prize?.id}>
-                    <a href={`/giveaways/${win.prize.giveaway?.id}`}>
-                        {win.prize?.prize_name}
-                    </a>
-                </li>
-            ))}
-            </ul>
-        ) : (
-            <p>No prizes found.</p>
-        )}
+      <h2>Your Prizes</h2>
+      {currentUser.prev_wins && currentUser.prev_wins.length > 0 ? (
+        <ul>
+          {currentUser.prev_wins.map((win) => (
+            <li key={win.prize?.id}>
+              <a href={`/giveaways/${win.prize.giveaway?.id}`}>
+                {win.prize?.prize_name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No prizes found.</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default UserPrizes
+export default UserPrizes;
